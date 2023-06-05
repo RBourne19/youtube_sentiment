3568003cc9a265eaa7160ab32132583f17c35433
RBourne19 <robertbourne100@gmail.com>
2023-06-04 23:08:50 -0400

    simplified

--- youtube.py
+++ youtube.py
@@ -7,8 +7,11 @@ dotenv.load_dotenv()
 api_key = os.getenv('API_KEY')
 def video_comments(video_id):
     count = 0
+    negative = 0 
+    positive = 0
+    neutral = 0
     # empty list for storing reply
-    response = {'negative': 0, 'positive': 0, 'neutral': 0, 'total': 0}
+    
  
     # creating youtube resource object
     youtube = build('youtube', 'v3',
@@ -29,16 +32,14 @@ def video_comments(video_id):
             # Extracting comments
             comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
             print(comment)
-            negative = SentimentIntensityAnalyzer().polarity_scores(comment)['neg']
-            positive = SentimentIntensityAnalyzer().polarity_scores(comment)['pos']
-            if(negative > positive):
-                response.update({'negative': response.get('negative') + 1})
-            elif negative != positive:
-                response.update({'positive': response.get('positive') + 1})
+            negativeScore = SentimentIntensityAnalyzer().polarity_scores(comment)['neg']
+            positiveScore = SentimentIntensityAnalyzer().polarity_scores(comment)['pos']
+            if(negativeScore > positiveScore):
+                negative += 1
+            elif negativeScore != positiveScore:
+                positive += 1
             else:
-                response.update({'neutral': response.get('neutral') + 1})
-            # counting number of reply of comment
-            replycount = item['snippet']['totalReplyCount']
+                neutral += 1
             count += 1    
         # Again repeat
         if 'nextPageToken' in video_response:
@@ -51,10 +52,10 @@ def video_comments(video_id):
         else:
             break
 
-    response.update({'total' : count})
-    return response
+    
+    return {'negative': negative, 'positive': positive, 'neutral': neutral, 'total': count}
 # Enter video id
-video_id = "B-tL7220WYc"
+video_id = "wdSXpQcjj0I"
  
 # Call function
 print(video_comments(video_id))
